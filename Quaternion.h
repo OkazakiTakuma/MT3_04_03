5a2a010d997f5a50f21ab19370ff6857cbf3628e
OkazakiTakuma <k024g0021@g.neec.ac.jp>
2025-11-18 14:42:20 +0900

    球面線形補間

--- Quaternion.h
+++ Quaternion.h
@@ -25,3 +25,5 @@ Quaternion MakeRotateAxisAngleQuaternion(const Vector3 axis, const float angle);
 Vector3 RotateVector(const Vector3& v, const Quaternion& q);
 
 Matrix4x4 MakeRotateMatrix(const Quaternion& q);
+
+Quaternion Slerp(const Quaternion& q1, const Quaternion& q2, float t);
